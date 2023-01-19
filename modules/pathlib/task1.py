7ec6517e0479ddd5de8f7014082d74f6fda16a6b
Сергій Жигун <109211611+SerhiiZhyhun77@users.noreply.github.com>
2023-01-19 16:23:17 +0200

    Add files via upload

--- modules/pathlib/task1.py
+++ modules/pathlib/task1.py
@@ -12,42 +12,60 @@
 
 import pathlib
 import random
+import shutil
 
 suffixes = ['.txt', '.doc', '.png', '.jpg', '.csv', '.gif']
 img_suffixes = ['.png', '.jpg', '.gif']
 
+# start
 print('*' * 15, 'START', '*' * 15)
-# create directory 'practice_files' if not exits
-print("Create directory 'practice_files'")
+
+# if exists directory 'practice_files' - remove it
+# create path
 path = pathlib.Path.cwd()
 practice_files = path / 'practice_files'
+
+# check and remove
+if practice_files.exists():
+    print("Directory 'practice_files' is exists. Remove it...")
+    shutil.rmtree(practice_files)
+
+# create new directory 'practice_files'
+print("Create directory 'practice_files'")
 practice_files.mkdir(exist_ok = True)
 
-# create directory 'documents' if not exists
+# create directory 'documents'
 print("Create directory 'documents'")
 documents = practice_files / 'documents'
 documents.mkdir(exist_ok = True)
 
-# create some random directories
-print("Create random directory:")
-number = random.randint(1, 10)
+# create some directories
+number = random.randint(2, 10)
+print(f"Create {number - 1} directories in 'documents':")
 for i in range(1, number):
     rand_dir_path = documents / f'dir{i}'
     rand_dir_path.mkdir(exist_ok = True)
-    print(rand_dir_path)
+    print(f'\t{rand_dir_path}')
+
 # create some files with random suffixes in random directories
 list_dir = list(documents.iterdir())  # create list all directories
-list_dir.append(documents)  # add 'documents' directory
-number = random.randint(1, 100)
+list_dir.append(documents)  # add 'documents' directory to the list
+number = random.randint(2, 100)
+print(f'Create {number - 1} files:')
 for i in range(1, number):
-    rand_suffix = random.choice(suffixes)
-    rand_dir_path = random.choice(list_dir)
-    rand_file_path = rand_dir_path / 'file{}{}'.format(i, rand_suffix)
+    rand_suffix = random.choice(suffixes)  # get random suffix
+    rand_dir_path = random.choice(list_dir)  # get random directory 
+    file_name = f'\tfile{i}{rand_suffix}'
+    rand_file_path = rand_dir_path / file_name
     rand_file_path.touch()
+    print(f'{file_name} => \n' \
+          f'in dir: {rand_dir_path}')
 
-input()
+# wait for key press
+input('Press any key for continue...')
+
 # create directory 'images'
-print("Create directory 'images'")
+print("Create directory 'images' in 'practice_files'")
 images = practice_files / 'images'
 images.mkdir(exist_ok = True)
 
@@ -57,6 +75,10 @@ for img_suffix in img_suffixes:
     img_paths += list(documents.rglob('*' + img_suffix))
 
 # move all image files
+print("Move images to 'images':")
 for source in img_paths:
     destination = images / source.name
-    source.replace(destination)    
+    source.replace(destination)
+    print(f'\t{source.name}')
+
+print('*' * 37)
