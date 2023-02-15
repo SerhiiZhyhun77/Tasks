b796645d436e4e9c229f444ff217bad6da912f06
Сергій Жигун <109211611+SerhiiZhyhun77@users.noreply.github.com>
2023-02-15 16:40:00 +0200

    Add files via upload

--- modules/PyPDF2/task10/task10.py
+++ modules/PyPDF2/task10/task10.py
@@ -6,7 +6,12 @@
 В текущей директории находится файл ugly.pdf, который содержит текст сказки
 Ганса Христиана Андерсена "Ugly Duckling", но каждая нечетная страница
 повернута на 90 градусов против часовой стрелки.
-Исправьте этот недостаток и сохраните в новом файле ugly_rotated.pdf.
+
+1. Исправьте этот недостаток повернув каждую четную страницу на 90 градусов и
+сохраните в файле ugly_rotated.pdf.
+
+2. Выполните это же задание используя для поворота ключ метаданных страницы
+/Rotate и сохраните результат в файл ugly_rotated2.pdf.
 """
 
 from pathlib import Path
@@ -14,6 +19,8 @@ from PyPDF2 import PdfReader, PdfWriter
 
 pdf_path = Path('ugly.pdf')
 
+# 1
+print('Task1:')
 # create necessary pdf instances
 pdf_reader = PdfReader(pdf_path)
 pdf_writer = PdfWriter()
@@ -26,4 +33,21 @@ for i, page in enumerate(pdf_reader.pages):
 # write result to file "ugly_rotated.pdf"
 print('Write to file "ugly_rotated.pdf"')
 with Path('ugly_rotated.pdf').open(mode='wb') as output_file:
+    pdf_writer.write(output_file)
+print()
+
+# 2
+print('Task2:')
+# create new pdf instances
+pdf_reader = PdfReader(pdf_path)
+pdf_writer = PdfWriter()
+# check metadata /Rotate. If its = -90 - rotate(90). Add page to new pdf.
+for page in pdf_reader.pages:
+    if (page['/Rotate']) == -90:
+        page.rotate(90)
+        print(f'\t-> Rotate {page["/StructParents"] + 1} page')
+    pdf_writer.add_page(page)
+# write result to file "ugly_rotated2.pdf"
+print('Write to file "ugly_rotated2.pdf"')
+with Path('ugly_rotated2.pdf').open(mode='wb') as output_file:
     pdf_writer.write(output_file)
