7e38932b1cf2cc7c4c32b67fafbc1300221446b6
Сергій Жигун <109211611+SerhiiZhyhun77@users.noreply.github.com>
2023-02-24 17:20:02 +0200

    Add files via upload

--- modules/reportlab/task2/task2.py
+++ modules/reportlab/task2/task2.py
@@ -15,15 +15,16 @@ from reportlab.lib.pagesizes import LETTER
 
 canvas = Canvas('font-example.pdf', pagesize=LETTER)
 
+# Times New Roman
 canvas.setFont('Times-Roman', 18)
 canvas.drawString(1 * inch, 10 * inch, 'Times New Roman (18pt)' )
-
+# Times New Roman Italic
 canvas.setFont('Times-Italic', 18)
 canvas.drawString(1 * inch, 9.5 * inch, 'Times New Roman Italic (18pt)')
-
+# Times New Roman Bold
 canvas.setFont('Times-Bold',18)
 canvas.drawString(1 * inch, 9 * inch, 'Times New Roman Bold (18pt)')
-
+# Times New Roman Bold Italic
 canvas.setFont('Times-BoldItalic', 18)
 canvas.drawString(1 * inch, 8.5 * inch, 'Times New Roman Bold Italic (18pt)')
 
