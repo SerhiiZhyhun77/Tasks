03d52ccfb2f0c9a54eb6af5b7223e986a4f9ba2f
Сергій Жигун <109211611+SerhiiZhyhun77@users.noreply.github.com>
2023-02-24 17:18:22 +0200

    Add files via upload

---
+++ modules/reportlab/task2/task2.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+
+"""
+[Знакомство с PYTHON. Дэн Бейдер, Дэвис Эймос (2023)]
+
+Создайте PDF файл font-exemple.pdf:
+    страница - размер LETTER,
+    4 строки написанные разными видами шрифта Times New Roman,
+    шрифт 18pt
+"""
+
+from reportlab.pdfgen.canvas import Canvas
+from reportlab.lib.units import inch, cm
+from reportlab.lib.pagesizes import LETTER
+
+canvas = Canvas('font-example.pdf', pagesize=LETTER)
+
+canvas.setFont('Times-Roman', 18)
+canvas.drawString(1 * inch, 10 * inch, 'Times New Roman (18pt)' )
+
+canvas.setFont('Times-Italic', 18)
+canvas.drawString(1 * inch, 9.5 * inch, 'Times New Roman Italic (18pt)')
+
+canvas.setFont('Times-Bold',18)
+canvas.drawString(1 * inch, 9 * inch, 'Times New Roman Bold (18pt)')
+
+canvas.setFont('Times-BoldItalic', 18)
+canvas.drawString(1 * inch, 8.5 * inch, 'Times New Roman Bold Italic (18pt)')
+
+canvas.save()
