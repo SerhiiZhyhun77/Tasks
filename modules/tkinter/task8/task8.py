9aba39e405d7f7108403888b5476db6c52e90d1e
Сергій Жигун <109211611+SerhiiZhyhun77@users.noreply.github.com>
2023-03-09 16:08:38 +0200

    Add files via upload

---
+++ modules/tkinter/task8/task8.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+
+"""
+[Знакомство с PYTHON. Дэн Бейдер, Дэвис Эймос (2023)]
+
+Создайте окно с виджетом Label, содержащим числовое значение. Слева и справа
+от виджета Label расположите кнопки. Левая кнопка должна уменьшать значение
+Label, а правая - увеличивать его.
+"""
+
+import tkinter as tk
+
+def increase():
+    value = int(lbl_value['text'])
+    lbl_value['text'] = f'{value + 1}'
+
+def decrease():
+    value = int(lbl_value['text'])
+    lbl_value['text'] = f'{value - 1}'
+
+window = tk.Tk()
+
+window.rowconfigure(0, minsize=50, weight=1)
+window.columnconfigure([0, 1, 2], minsize=50, weight=1)
+
+btn_decrease = tk.Button(master=window, text='-', command=decrease)
+btn_decrease.grid(row=0, column=0,sticky='nsew')
+
+lbl_value = tk.Label(master=window, text=0)
+lbl_value.grid(row=0, column=1)
+
+btn_increase = tk.Button(master=window, text='+', command=increase)
+btn_increase.grid(row=0, column=2, sticky='nsew')
+
+window.mainloop()
