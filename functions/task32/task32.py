3436e92f58c8f7bd9c2968364cafc00cd52f81e7
Сергій Жигун <109211611+SerhiiZhyhun77@users.noreply.github.com>
2023-04-01 17:45:27 +0300

    Add files via upload

---
+++ functions/task32/task32.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+
+"""ЗАДАЧА: СЧЕТЧИК ЧЕТНЫХ / НЕЧЕТНЫХ ЧИСЕЛ
+[Начинаем программировать на Python. Тони Гэддис (2022)]
+
+Напишите программу, которая генерирует 100 случайных чисел и подсчитывает
+количество четных и нечетных случайных чисел.
+"""
+
+import random
+
+def is_even(number):
+    return True if number % 2 == 0 else False
+
+def main():
+    numbers_odd = []
+    numbers_even = []
+    for i in range(100):
+        num = random.randint(1, 100)
+        if is_even(num):
+            numbers_even.append(num)
+        else:
+            numbers_odd.append(num)
+    print('Even numbers:')
+    print(numbers_even)
+    print(f'Number of even numbers: {len(numbers_even)}')
+    print()
+    print('Odd numbers:')
+    print(numbers_odd)
+    print(f'Number of odd numbers: {len(numbers_odd)}')
+
+if __name__ == '__main__':
+    main()
