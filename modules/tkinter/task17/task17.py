f64d56acf9644672c1062e8d457e94e8506e7232
serhii <sergei.zhigun@gmail.com>
2023-07-23 17:09:21 +0300

    modules/tkinter/task17 completed

--- modules/tkinter/task17/task17.py
+++ modules/tkinter/task17/task17.py
@@ -12,3 +12,61 @@
 
     Показатель миль на галлон = мили / галлоны
 '''
+
+import tkinter as tk
+
+
+class GUI:
+
+    def __init__(self):
+        # Main window
+        self.window = tk.Tk()
+        self.window.title('Calc MPG')
+        self.window.geometry('150x180')
+        # Frames
+        self.ent_frame = tk.Frame(self.window)
+        self.btn_frame = tk.Frame(self.window)
+        # Labels + Entries
+        self.value = tk.StringVar()
+        self.lbl_gal = tk.Label(self.ent_frame, text='Gallons of gasoline')
+        self.ent_gal = tk.Entry(self.ent_frame, width=10)
+        self.lbl_miles = tk.Label(self.ent_frame, text='Miles')
+        self.ent_miles = tk.Entry(self.ent_frame, width=10)
+        self.lbl_res = tk.Label(self.ent_frame,
+                                textvariable=self.value,
+                                borderwidth=1,
+                                relief='ridge', width=6)
+        self.lbl_prompt = tk.Label(self.ent_frame, text='MPG')
+        # Button
+        self.btn_calc = tk.Button(self.btn_frame,
+                                  text='Calc MPG',
+                                  command=self.calc)
+        self.lbl_gal.pack()
+        self.ent_gal.pack()
+        self.lbl_miles.pack()
+        self.ent_miles.pack()
+        self.lbl_res.pack(side='left', padx=(22, 5), pady=15)
+        self.lbl_prompt.pack(side='left')
+        self.btn_calc.pack(side='bottom', pady=(0, 5))
+        self.ent_frame.pack()
+        self.btn_frame.pack()
+        # Mainloop
+        self.window.mainloop()
+
+    def calc(self):
+        gal = self.ent_gal.get()
+        miles = self.ent_miles.get()
+        try:
+            gal = float(gal)
+            miles = float(miles)
+            self.value.set(f'{miles / gal:.2f}')
+        except Exception:
+            self.value.set('Error')
+
+
+def main():
+    gui = GUI()
+
+
+if __name__ == '__main__':
+    main()
