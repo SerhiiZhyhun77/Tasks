08ed274cb09c82b6713ba720231b72031fb589cb
serhii <sergei.zhigun@gmail.com>
2024-06-09 18:41:30 +0300

    test for lerning

--- test/1.py
+++ test/1.py
@@ -1,17 +1,31 @@
 import random
-secret = "Я задумав число від 1 до 1000"
-case = random.randint(1, 1000)
-print(secret)
-attempt = 0
-_input = 0
-while _input != case:
-    attempt += 1
-    print("Вгадай число: ", end="")
-    _input = int(input())
-    if _input < case:
-        print("Мало")
-    if _input > case:
-        print("Багато")
-    if _input == case:
-        print("Вгадав!")
-print(f"Вгадано за {attempt} спроб.")
+import time
+print("Давай кинемо кубики!")
+your_number = 0
+my_number = 0
+
+# Гральні кубики
+for n in range(100):
+    print(str(n + 1) + ". Раунд")
+    print("Твій кидок: ", end="")
+    shoot1 = random.randint(1, 6)
+    time.sleep(0.5)
+    print(shoot1)
+    print("Мій кидок: ", end="")
+    shoot2 = random.randint(1, 6)
+    time.sleep(0.5)
+    print(shoot2)
+    if shoot1 > shoot2:
+        your_number += 1
+    if shoot1 < shoot2:
+        my_number += 1
+    print(str(your_number) + " і " + str(my_number))
+    time.sleep(1)
+    print()
+
+if your_number > my_number:
+    print("Ти виграв")
+elif your_number < my_number:
+    print("Я виграв")
+else:
+    print("Нічия")
