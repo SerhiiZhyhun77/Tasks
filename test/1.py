f69f86c1ea17858e6f466a9cdf661b9c59eaedb1
serhii <sergei.zhigun@gmail.com>
2024-06-09 16:47:24 +0300

    test for lerning

--- test/1.py
+++ test/1.py
@@ -2,8 +2,10 @@ import random
 secret = "Я задумав число від 1 до 1000"
 case = random.randint(1, 1000)
 print(secret)
+attempt = 0
 _input = 0
 while _input != case:
+    attempt += 1
     print("Вгадай число: ", end="")
     _input = int(input())
     if _input < case:
@@ -12,3 +14,4 @@ while _input != case:
         print("Багато")
     if _input == case:
         print("Вгадав!")
+print(f"Вгадано за {attempt} спроб.")
