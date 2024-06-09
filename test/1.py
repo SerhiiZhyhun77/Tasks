883ad8519e013badafe9c8ba6710464e82974692
serhii <sergei.zhigun@gmail.com>
2024-06-09 13:30:56 +0300

    test for lerning

--- test/1.py
+++ test/1.py
@@ -1,4 +1,8 @@
-Text = "Hello! Who are you?"
+Text = "Привіт! Ти хто?"
 print(Text)
 Name = input()
-print(Name)
+Text = "Так значить, ти - " + Name
+print(Text)
+print("А як у тебе справи?")
+Answer = input()
+print("Я радий, що у тебе справи " + Answer)
