d5a49e02ad1433d74743fd3c761331f30df53cf1
serhii <sergei.zhigun@gmail.com>
2024-06-09 14:43:02 +0300

    test for lerning

--- test/1.py
+++ test/1.py
@@ -1,8 +1,14 @@
-Text = "Привіт! Ти хто?"
-print(Text)
-Name = input()
-Text = "Так значить, ти - " + Name
-print(Text)
-print("А як у тебе справи?")
-Answer = input()
-print("Я радий, що у тебе справи " + Answer)
+import random
+secret = "Я задумав число від 1 до 1000"
+case = random.randint(1, 1000)
+print(secret)
+_input = 0
+while _input != case:
+    print("Вгадай число: ", end="")
+    _input = int(input())
+    if _input < case:
+        print("Мало")
+    if _input > case:
+        print("Багато")
+    if _input == case:
+        print("Вгадав!")
