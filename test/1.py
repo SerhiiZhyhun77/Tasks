536461872ac4ef8b5633615d3504454084bd7731
serhii <sergei.zhigun@gmail.com>
2024-06-15 18:02:24 +0300

    test for lerning

--- test/1.py
+++ test/1.py
@@ -1,36 +1,25 @@
 from tkinter import *
 
-
-def button_click1():
-    display.configure(text="Это здорово!")
-def button_click2():
-    display.configure(text="Это радует!")
-def button_click3():
-    display.configure(text="Все возможно.")
-def button_click4():
-    display.configure(text="Это огорчает!")
-def button_click5():
-    display.configure(text="Это плохо!")
-def button_click6():
-    display.configure(text="Раз ты так думаешь...")
+answer = ["Супер", "Хорошо", "Так себе", "Плохо", "Ужасно", "Не скажу"]
+diagnose = ["Это здорово!", "Это радует!", "Все возможно.", "Это огорчает!",
+            "Это плохо!", "Раз ты так думаешь..."]
 
 
+def listbox_select(event):
+    select = box.curselection()
+    nr = select[0]
+    display.config(text=diagnose[nr])
 
 window = Tk()
-window.config(width=300, height=190)
+window.title("Привет!")
+window.config(width=260, height=230)
 display = Label(window, text="Как это сделать?")
-display.place(x=80, y=10, width=160, height=30)
-button1 = Button(window, text="Супер", command=button_click1)
-button2 = Button(window, text="Хорошо", command=button_click2)
-button3 = Button(window, text="Так себе", command=button_click3)
-button4 = Button(window, text="Плохо", command=button_click4)
-button5 = Button(window, text="Ужасно", command=button_click5)
-button6 = Button(window, text="Не скажу", command=button_click6)
-button1.place(x=20, y=60, width=120, height=30 )
-button2.place(x=160, y=60, width=120, height=30)
-button3.place(x=20, y=100, width=120, height=30)
-button4.place(x=160, y=100, width=120, height=30)
-button5.place(x=20, y=140, width=120, height=30)
-button6.place(x=160, y=140, width=120, height=30)
+display.place(x=20, y=10, width=160, height=30)
+
+box = Listbox(window)
+for nr in range(0,6):
+    box.insert(nr, answer[nr])
+box.bind("<<ListboxSelect>>", listbox_select)
+box.place(x=30, y=50, width=200, height=150)
 
 window.mainloop()
