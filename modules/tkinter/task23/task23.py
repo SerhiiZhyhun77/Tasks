0da2ad47944c9f49cc518afdeec534371d90d90c
serhii <sergei.zhigun@gmail.com>
2024-06-16 16:22:38 +0300

    Added new elements to the form

--- modules/tkinter/task23/task23.py
+++ modules/tkinter/task23/task23.py
@@ -17,15 +17,26 @@ window = Tk()
 window.title("Psychologist")
 window.minsize(width=500, height=330)
 
+
 border = []
+display = []
 for pos in range(0, 3):
     border.append(Frame(window, borderwidth=2, relief='groove'))
     border[pos].place(x=20, y=40 + pos * 90, width=460, height=50)
-
-display = []
-for pos in range(0, 3):
     display.append(Label(window, text=state[pos]))
     display[pos].place(x=20, y=10 + pos * 90, width=460, height=30)
+
+_input = Entry(border[0])
+_input.place(x=10, y=10, width=440, height=30)
+
+answer = Label(border[1])
+answer.place(x=10, y=10, width=440, height=30)
+
+slider = Scale(border[2], orient="horizontal")
+slider.config(length=430, showvalue=0)
+slider.pack(pady=10)
+
+
 
 
 button1 = Button(window, text="Again", command=button1_click)
