4cc5ee88ddf5603add8b8c8614c13742f48f745b
serhii <sergei.zhigun@gmail.com>
2024-06-16 17:11:27 +0300

    Fully working program

--- modules/tkinter/task23/task23.py
+++ modules/tkinter/task23/task23.py
@@ -1,23 +1,35 @@
 # Psychologist program
 from tkinter import *
+import random
 
 # text constant
 state = ["You said me:", "I tell you:", "Diagnosis:"]
 diagnose = ["This is awesome!", "This makes me happy!", "Everything is possible.",
             "This is upsetting!", "This is bad!", "If you think so..."]
 
+
 # function for events
 def button1_click():
-    pass
+    _input.delete(0, "end")
+    answer.config(text="")
+
+
 def button2_click():
-    pass
+    nr = random.randint(0, 5)
+    answer.config(text=diagnose[nr])
+
+
+def scale_value(event):
+    nr = slider.get()
+    answer.config(text=diagnose[nr])
+
 
 # main program
 window = Tk()
 window.title("Psychologist")
 window.minsize(width=500, height=330)
 
-
+# frames & labels
 border = []
 display = []
 for pos in range(0, 3):
@@ -26,23 +38,21 @@ for pos in range(0, 3):
     display.append(Label(window, text=state[pos]))
     display[pos].place(x=20, y=10 + pos * 90, width=460, height=30)
 
+# input & lbl for answer
 _input = Entry(border[0])
 _input.place(x=10, y=10, width=440, height=30)
-
 answer = Label(border[1])
 answer.place(x=10, y=10, width=440, height=30)
 
-slider = Scale(border[2], orient="horizontal")
-slider.config(length=430, showvalue=0)
+# slider
+slider = Scale(border[2], orient="horizontal", command=scale_value)
+slider.config(from_=0, to=5, length=430, showvalue=False)
 slider.pack(pady=10)
 
-
-
-
+# buttons
 button1 = Button(window, text="Again", command=button1_click)
 button1.place(x=120, y=285, width=120, height=30)
 button2 = Button(window, text="Done", command=button2_click)
 button2.place(x=260, y=285, width=120, height=30)
-
 
 window.mainloop()
