f1c6dd868c66e1b0346a3a8f43045c1d59df3b9e
serhii <sergei.zhigun@gmail.com>
2024-06-17 23:51:58 +0300

    test for learning

--- test/1.py
+++ test/1.py
@@ -7,7 +7,7 @@ window.title("Черепашка")
 window.setup(width=width, height=height)
 
 shape("turtle")
-for step in range(200):
+for step in range(20):
     forward(random.randint(5, 30))
     left(random.randint(-90, 90))
 
