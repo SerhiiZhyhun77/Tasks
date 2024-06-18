cb43ca89ff0be1fde990b8b6a4f0d4e09711541b
serhii <sergei.zhigun@gmail.com>
2024-06-18 19:42:46 +0300

    test for learning

--- test/1.py
+++ test/1.py
@@ -3,15 +3,23 @@ from tkinter import *
 width = 500
 height = 330
 mode = ["Появление", "Движение", "Сокрытие"]
+x, y, z = 20, 50, 200
 
 def show_image():
-    pass
+    global circle
+    circle=graphic.create_oval(x, y, x + z, y + z, fill="red")
 
 def move_image():
-    pass
+    global circle
+    for pos in range(20, width - 220, 2):
+        graphic.move(circle, 2, 0)
+        graphic.update()
+        graphic.after(20)
+
 
 def hide_image():
-    pass
+    global circle
+    graphic.delete(circle)
 
 window = Tk()
 window.title("Анимация")
@@ -22,7 +30,7 @@ graphic.pack()
 knob = []
 for nr in range(3):
     knob.append(Button(window, text=mode[nr]))
-    knob.place(x=30 + nr *150, y=height -50, width=140, height=30)
+    knob[nr].place(x=30 + nr *150, y=height - 50, width=140, height=30)
 knob[0].config(command=show_image)
 knob[1].config(command=move_image)
 knob[2].config(command=hide_image)
