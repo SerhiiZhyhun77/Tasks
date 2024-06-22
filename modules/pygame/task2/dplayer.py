bab7267ef64781a5f4cd781facd15ac129346c4e
serhii <sergei.zhigun@gmail.com>
2024-06-22 16:37:37 +0300

    task2. module pygame

---
+++ modules/pygame/task2/dplayer.py
@@ -0,0 +1,19 @@
+import pygame as pg
+
+
+class Player(pg.sprite.Sprite):
+    image = []
+
+    def __init__(self, x_pos=0, y_pos=0):
+        super().__init__()
+        self.image.append(pg.image.load("images/do_stand.png"))
+        self.image.append(pg.image.load("images/do_duck.png"))
+        self.image.append(pg.image.load("images/do_jump.png"))
+        self.x, self.y = x_pos, y_pos
+        self.bild = self.image[0]
+        self.is_hit = False
+        self.status = 0
+
+    def set_state(self, nr):
+        self.status = nr
+        self.bild = self.image[nr]
