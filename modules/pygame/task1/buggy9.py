a1c32cab5344f6314fa6390f4802ac270ac6fc17
serhii <sergei.zhigun@gmail.com>
2024-06-23 18:27:26 +0300

    task2. module pygame finished

--- modules/pygame/task1/buggy9.py
+++ modules/pygame/task1/buggy9.py
@@ -9,20 +9,33 @@ x_max, y_max = 600, 400
 direction = 0
 distance = 0
 degree = 0
+bug_max = 5
 
 # launching Pygame, creating a game field and character
 pg.init()
 pg.key.set_repeat(20, 20)
 pg.display.set_caption("Bug Game")
 window = pg.display.set_mode((x_max, y_max))
-figure = Player(x_max / 2 - 50, y_max / 2 - 50)
+
+figure = []
+for nr in range(0, bug_max):
+    x_pos = random.randint(100, x_max - 100)
+    y_pos = random.randint(50, y_max - 100)
+    figure.append(Player(x_pos, y_pos))
+
 
 # random direction
-x_step, y_step = random.randint(0, 2), random.randint(0, 2)
-if x_step == 0:
-    x_step = -1
-if y_step == 0:
-    y_step = -1
+x_step = []
+y_step = []
+
+for nr in range(0, bug_max):
+    x_step.append(random.randint(0, 2))
+    if x_step[nr] == 0:
+        x_step[nr] = -1
+    y_step.append(random.randint(0, 2))
+    if y_step[nr] == 0:
+        y_step[nr] = -1
+
 
 # event loop
 running = True
@@ -34,9 +47,12 @@ while running:
         # setting the mouse
         if event.type == pg.MOUSEBUTTONDOWN:
             (x_pos, y_pos) = pg.mouse.get_pos()
-            if x_pos > figure.x and x_pos < figure.x + 100 \
-                and y_pos > figure.y and y_pos < figure.y + 100:
-                figure.destroy()
+
+
+            for nr in range (0, bug_max):
+                if x_pos > figure[nr].x and x_pos < figure[nr].x + 100 \
+                    and y_pos > figure[nr].y and y_pos < figure[nr].y + 100:
+                    figure[nr].destroy()
 
     # restriction of movement
     if figure.x < 0 or figure.x > x_max - 110:
