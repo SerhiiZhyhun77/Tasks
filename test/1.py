d19be18a4dd331a0dbcd0a817f643d48511221bb
serhii <sergei.zhigun@gmail.com>
2024-06-25 23:20:23 +0300

    test for learning

--- test/1.py
+++ test/1.py
@@ -1,9 +1,7 @@
-import turtle
-colors = ['red', 'yellow', 'blue', 'green']
-turtle.bgcolor('black')
-t = turtle.Pen()
-t.speed(0)
-for x in range(500):
-    t.pencolor(colors[x % 4])
-    t.circle(x)
-    t.left(91)
+my_name = "Брайсон"
+my_age = 43
+your_name = input("Как тебя зовут? ")
+your_age = input("Сколько тебе лет? ")
+print("Меня зовут", my_name, ", и мне", my_age, "лет.")
+print("Тебя зовут", your_name, ", и тебе", your_age, ".")
+print("Спасибо за покупку моей книги,", your_name, "!")
