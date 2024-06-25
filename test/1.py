fcc42ad08f50768ee7a847ad3a6b6ceb79fabdc3
serhii <sergei.zhigun@gmail.com>
2024-06-25 21:58:36 +0300

    test for learning

--- test/1.py
+++ test/1.py
@@ -1,5 +1,8 @@
 import turtle
+colors = ['red', 'yellow', 'blue', 'green']
 t = turtle.Pen()
+t.speed(0)
 for x in range(100):
-    t.circle(x)
+    t.pencolor(colors[x % 4])
+    t.forward(x)
     t.left(91)
