e301349e14047bbd353d12fa1766232eca42ca12
serhii <sergei.zhigun@gmail.com>
2024-07-05 22:41:56 +0300

    test for learning

--- test/1.py
+++ test/1.py
@@ -1,7 +1,18 @@
-name = input("Как тебя зовут? ")
-while name != "":
-    for x in range(100):
-        print(name, end=' ')
-    print()
-    name = input("Введите еще имя или нажмите [Enter], чтобы выйти: ")
-print('Спасибо за игру!')
+import turtle
+t = turtle.Pen()
+turtle.bgcolor('black')
+colors = ['red', 'yellow', 'blue', 'green', 'orange', 'purple','white',
+          'brown', 'gray', 'pink']
+family = []
+name = turtle.textinput('Моя семья', 'Введите имя или нажмите [Enter], чтобы выйти:')
+while name != '':
+    family.append(name)
+    name = turtle.textinput('Моя семья', 'Введите имя или нажмите [Enter], чтобы выйти:')
+
+for x in range(100):
+    t.pencolor(colors[x % len(family)])
+    t.penup()
+    t.forward(x * 4)
+    t.pendown()
+    t.write(family[x % len(family)], font=('Arial', int((x + 4) / 4), 'bold'))
+    t.left(360 / len(family) + 2)
