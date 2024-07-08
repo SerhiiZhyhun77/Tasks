4481ceb81e85cd4e8e503a234f706c5805f5def6
serhii <sergei.zhigun@gmail.com>
2024-07-08 17:38:29 +0300

    test for learning

--- test/1.py
+++ test/1.py
@@ -1,23 +1,20 @@
 import turtle
 t = turtle.Pen()
-t.penup()
-turtle.bgcolor('black')
-sides = int(turtle.numinput('Количество сторон', 'Сколько сторон у вашей спирали спиралей? (2-6)', 4, 2, 6))
-colors = ['red', 'yellow', 'blue', 'green', 'orange', 'purple','white',
-          'brown', 'gray', 'pink']
-for m in range(100):
+t.speed(0)
+sides = int(turtle.numinput("Количество сторон", "Сколько сторон будет у вашей спирали?", 4))
+for m in range(5, 75):
+    t.left(360 / sides + 5)
+    t.width(m // 25 + 1)
+    t.penup()
     t.forward(m * 4)
-    position = t.position()
-    heading = t.heading()
-    print(position, heading)
+    t.pendown()
 
-    for n in range(int(m / 2)):
-        t.pendown()
-        t.pencolor(colors[n % sides])
-        t.forward(2 * n)
-        t.right(360 / sides - 2)
-        t.penup()
-    t.setx(position[0])
-    t.sety(position[1])
-    t.setheading(heading)
-    t.left(360 / sides + 2)
+    if (m % 2 == 0):
+        for m in range(sides):
+            t.circle(m / 3 + 3)
+            t.right(360 / sides)
+    else:
+        for m in range(sides):
+            t.forward(m + 3)
+            t.right(360 / sides)
+turtle.exitonclick()
