656709481ce5829a8ad5225a35e6051ac574a6fc
serhii <sergei.zhigun@gmail.com>
2024-07-08 20:23:31 +0300

    test for learning

--- test/1.py
+++ test/1.py
@@ -1,12 +1,10 @@
-message = input("Введите сообщение для кодирования или раскодирования: ")
-message = message.upper()
-output = ""
-for letter in message:
-    if letter.isupper():
-        value = ord(letter) + 13
-        letter = chr(value)
-        if not letter.isupper():
-            value -= 26
-            letter = chr(value)
-    output += letter
-print("Выходное сообщение: ", output)
+import random
+the_number = random.randint(1, 10)
+guess = int(input("Угадайте число от 1 до 10: "))
+while guess != the_number:
+    if guess > the_number:
+        print(guess, "Слишком велико. Попробуйте снова.")
+    if guess < the_number:
+        print(guess, "Слишком мало. Попробуйте снова.")
+    guess = int(input("Еще одна попытка: "))
+print(guess, "Правильное число! Вы победили!")
