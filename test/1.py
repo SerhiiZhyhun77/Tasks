354827f39ad55fc4665581fdd213fc2db3b20ba2
serhii <sergei.zhigun@gmail.com>
2024-07-08 20:10:42 +0300

    test for learning

--- test/1.py
+++ test/1.py
@@ -1,20 +1,12 @@
-import turtle
-t = turtle.Pen()
-t.speed(0)
-sides = int(turtle.numinput("Количество сторон", "Сколько сторон будет у вашей спирали?", 4))
-for m in range(5, 75):
-    t.left(360 / sides + 5)
-    t.width(m // 25 + 1)
-    t.penup()
-    t.forward(m * 4)
-    t.pendown()
-
-    if (m % 2 == 0):
-        for m in range(sides):
-            t.circle(m / 3 + 3)
-            t.right(360 / sides)
-    else:
-        for m in range(sides):
-            t.forward(m + 3)
-            t.right(360 / sides)
-turtle.exitonclick()
+message = input("Введите сообщение для кодирования или раскодирования: ")
+message = message.upper()
+output = ""
+for letter in message:
+    if letter.isupper():
+        value = ord(letter) + 13
+        letter = chr(value)
+        if not letter.isupper():
+            value -= 26
+            letter = chr(value)
+    output += letter
+print("Выходное сообщение: ", output)
