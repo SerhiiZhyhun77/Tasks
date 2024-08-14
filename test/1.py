8c31136700be3dec2fc698589c97caa62d21563d
serhii <sergei.zhigun@gmail.com>
2024-08-14 22:09:00 +0300

    test for learning

--- test/1.py
+++ test/1.py
@@ -3,37 +3,49 @@ import turtle
 t = turtle.Pen()
 t.speed(0)
 turtle.bgcolor("black")
-colors = ['red', 'yellow', 'blue', 'orange', 'purple', 'white', 'gray']
-for n in range(50):
-    t.pencolor(random.choice(colors))
-    size = random.randint(10, 40)
-    x = random.randrange(0, turtle.window_width() // 2)
-    y = random.randrange(0, turtle.window_height() // 2)
 
+def draw_smiley(x, y):
     t.penup()
     t.setpos(x, y)
     t.pendown()
-    for m in range(size):
-        t.forward(m * 2)
-        t.left(91)
+    # face
+    t.pencolor('green')
+    t.fillcolor('yellow')
+    t.begin_fill()
+    t.circle(50)
+    t.end_fill()
 
+    # left eye
     t.penup()
-    t.setpos(-x, y)
+    t.setpos(x - 15, y + 60)
     t.pendown()
-    for m in range(size):
-        t.forward(m * 2)
-        t.left(91)
-    t.penup()
-    t.setpos(-x, -y)
-    t.pendown()
-    for m in range(size):
-        t.forward(m * 2)
-        t.left(91)
-    t.penup()
-    t.setpos(x, -y)
-    t.pendown()
-    for m in range(size):
-        t.forward(m * 2)
-        t.left(91)
+    t.fillcolor('blue')
+    t.begin_fill()
+    t.circle(10)
+    t.end_fill()
 
+    # right eye
+    t.penup()
+    t.setpos(x + 15, y + 60)
+    t.pendown()
+    t.begin_fill()
+    t.circle(10)
+    t.end_fill()
+
+    # mouth
+    t.penup()
+    t.setpos(x - 25, y + 40)
+    t.pendown()
+    t.pencolor('black')
+    t.width(10)
+    t.goto(x - 10, y + 20)
+    t.goto(x + 10, y + 20)
+    t.goto(x + 25, y + 40)
+    t.width(1)
+
+
+for n in range(50):
+    x = random.randrange(-turtle.window_width() // 2, turtle.window_width() // 2)
+    y = random.randrange(-turtle.window_height() // 2, turtle.window_height() // 2)
+    draw_smiley(x, y)
 turtle.exitonclick()
