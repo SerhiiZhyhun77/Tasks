3e1e4564d72d08b8e25d50aefa6de4d74e69c0af
serhii <sergei.zhigun@gmail.com>
2024-08-15 19:13:42 +0300

    test for learning

--- test/1.py
+++ test/1.py
@@ -1,24 +1,27 @@
+import random
 import turtle
 t = turtle.Pen()
 t.speed(0)
+t.hideturtle()
 turtle.bgcolor('black')
-t.pencolor('white')
-t.turtlesize(2, 2, 20)
+colors = ['red', 'yellow', 'blue', 'green', 'orange', 'purple', 'white', 'gray']
 
-def up():
-    t.forward(50)
+def draw_kaleido(x, y):
+    t.pencolor(random.choice(colors))
+    size = random.randint(10, 40)
+    draw_spiral(x, y, size)
+    draw_spiral(-x, y, size)
+    draw_spiral(-x, -y, size)
+    draw_spiral(x, -y, size)
 
-def left():
-    t.left(90)
+def draw_spiral(x, y, size):
+    t.penup()
+    t.setpos(x, y)
+    t.pendown()
+    for m in range(size):
+        t.forward(m * 2)
+        t.left(92)
 
-def right():
-    t.right(90)
-
-
-turtle.onkeypress(up, 'Up')
-turtle.onkeypress(left, 'Left')
-turtle.onkeypress(right, 'Right')
-turtle.onscreenclick(t.setpos)
-turtle.listen()
+turtle.onscreenclick(draw_kaleido)
 
 turtle.mainloop()
