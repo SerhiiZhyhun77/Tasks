47c53e0f6f3e20f6f8814ea11b236b3d016b223e
serhii <sergei.zhigun@gmail.com>
2024-08-15 17:11:08 +0300

    test for learning

--- test/1.py
+++ test/1.py
@@ -1,52 +1,24 @@
-import random
 import turtle
 t = turtle.Pen()
 t.speed(0)
-t.hideturtle()
-turtle.bgcolor("black")
+turtle.bgcolor('black')
+t.pencolor('white')
+t.turtlesize(2, 2, 20)
 
-def draw_smiley(x, y):
-    t.penup()
-    t.setpos(x, y)
-    t.pendown()
-    # face
-    t.pencolor('green')
-    t.fillcolor('yellow')
-    t.begin_fill()
-    t.circle(50)
-    t.end_fill()
+def up():
+    t.forward(50)
 
-    # left eye
-    t.penup()
-    t.setpos(x - 15, y + 60)
-    t.pendown()
-    t.fillcolor('blue')
-    t.begin_fill()
-    t.circle(10)
-    t.end_fill()
+def left():
+    t.left(90)
 
-    # right eye
-    t.penup()
-    t.setpos(x + 15, y + 60)
-    t.pendown()
-    t.begin_fill()
-    t.circle(10)
-    t.end_fill()
-
-    # mouth
-    t.penup()
-    t.setpos(x - 25, y + 40)
-    t.pendown()
-    t.pencolor('black')
-    t.width(10)
-    t.goto(x - 10, y + 20)
-    t.goto(x + 10, y + 20)
-    t.goto(x + 25, y + 40)
-    t.width(1)
+def right():
+    t.right(90)
 
 
-for n in range(50):
-    x = random.randrange(-turtle.window_width() // 2, turtle.window_width() // 2)
-    y = random.randrange(-turtle.window_height() // 2, turtle.window_height() // 2)
-    draw_smiley(x, y)
-turtle.exitonclick()
+turtle.onkeypress(up, 'Up')
+turtle.onkeypress(left, 'Left')
+turtle.onkeypress(right, 'Right')
+turtle.onscreenclick(t.setpos)
+turtle.listen()
+
+turtle.mainloop()
