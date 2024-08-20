90e4260556fa151f996e1949c035a2ce1484ab58
serhii <sergei.zhigun@gmail.com>
2024-08-20 21:09:03 +0300

    pygame task4 completed

--- modules/pygame/task4/task4.py
+++ modules/pygame/task4/task4.py
@@ -1,4 +1,5 @@
-# Намалюйте 100 кіл випадкових кольорів випадкових розмірів у випадкових місцях
+# Намалюйте 100 кіл випадкових кольорів випадкових розмірів у випадкових місцях і примусьте їх рухатись згори до низу
+# зліва направо. Коли коло досягатиме краю екрану, нехай воно з'явиться вгорі і продовжить падіння.
 
 import random
 import pygame
@@ -10,6 +11,8 @@ colors = list([0] * 100)
 locations = list([0] * 100)
 sizes = list([0] * 100)
 keep_going = True
+BLACK = (0, 0, 0)
+timer = pygame.time.Clock()
 
 for n in range(100):
     colors[n] = (random.randint(0, 255),
@@ -17,16 +20,25 @@ for n in range(100):
                  random.randint(0, 255))
     locations[n] = (random.randint(0, 800),
                     random.randint(0, 600))
-    sizes[n] = random.randint(10, 100)
+    sizes[n] = random.randint(10, 40)
 
 while keep_going:
+    screen.fill(BLACK)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             keep_going = False
 
     for n in range(100):
         pygame.draw.circle(screen, colors[n], locations[n], sizes[n])
+        new_x = locations[n][0] + 1
+        new_y = locations[n][1] + 1
+        if new_x > 850:
+            new_x -= 900
+        if new_y > 650:
+            new_y -= 700
+        locations[n] = (new_x, new_y)
 
     pygame.display.update()
+    timer.tick(60)
 
 pygame.quit()
